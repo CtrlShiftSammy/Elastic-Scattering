3d097e082f9cb14b0c4b4a0fb448500a2b935c95
CtrlShiftSammy <CtrlShiftSammy@users.noreply.github.com>
2021-08-29 16:24:44 +0530

    update

--- Iteration 03/Elastic.py
+++ Iteration 03/Elastic.py
@@ -4,7 +4,6 @@ from typing import Coroutine
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-
 input_file = open("Iteration 03/elastic.in", "r")
 en = (float(input_file.readline().strip()) / 27.211)
 rmu = float(input_file.readline().strip())
@@ -53,7 +52,6 @@ v = 0.0
 rho = ren * p1
 tol = 1.0e-04
 maximum = 50
-
 def pot(r):
     global nmod, zmod, eta, xi, zasy, rmod
     if r < rmod:
@@ -61,7 +59,6 @@ def pot(r):
     else:
         v = zasy / r
     return v
-
 data_file = open("Iteration 03/density_xe+2.dat", "r")
 data = np.empty(shape = (182, 4))
 for i in range(0, 182):
@@ -74,7 +71,6 @@ for i in range(1, 182):
     data[i, 2] = data[i-1, 2] + 4 * pi * ((data[i, 0]) ** 3 - (data[i-1, 0]) ** 3) * (data[i, 1] + data[i-1, 1]) / 6.0
     data[i, 3] = (zmod - data[i, 2]) / data[i, 0]
 data_file.close()
-
 def pot_data(r):
     global data
     flag = False
@@ -83,7 +79,6 @@ def pot_data(r):
             v = data[i-1, 3] + (r - data[i-1, 0]) * (data[i, 3] - data[i-1, 3]) / (data[i, 0] - data[i-1, 0])
             flag = True
             return v
-
 def cgamma(z):
     cof = np.array([76.18009173e0, -86.50532033e0, 24.01409822e0, -1.231739516e0, 1.20858003e-3, -5.36382e-6])
     if z.real < 1:
@@ -100,7 +95,14 @@ def cgamma(z):
             cser = cser + cof[i] / (i + z)
         G = cser * ctmp * math.sqrt(2 * pi)
         return G
-
+def cgammaln(z):
+    cof = np.array([57.1562356658629235,-59.5979603554754912,14.1360979747417471,-0.491913816097620199,.339946499848118887e-4,.465236289270485756e-4,-.983744753048795646e-4,.158088703224912494e-3,-.210264441724104883e-3,.217439618115212643e-3,-.164318106536763890e-3,.844182239838527433e-4,-.261908384015814087e-4,.368991826595316234e-5])
+    cser = 0.999999999999997092
+    ctmp = (z+0.5)*cmath.log(z+5.24218750000000000)-(z+5.24218750000000000)
+    for i in range(0, 14):
+        cser = cser + cof[i] / (z + 1 + i)
+    lnG = ctmp+cmath.log(2.5066282746310005*cser/z)
+    return cmath.exp(lnG)
 def hyper(i, rho, eta, sigmal):
     thetal = rho - eta*math.log(2*rho) - i*pi*0.5 + sigmal
  
@@ -149,7 +151,6 @@ def hyper(i, rho, eta, sigmal):
     dghyp = f2 * c - g2 * s
     
     return fhyp, ghyp, dfhyp, dghyp
-
 def pl(lin, x):
     pl = 1.0 if lin == 0 else x
     plm2 = 1
@@ -159,7 +160,6 @@ def pl(lin, x):
         plm2 = plm1
         plm1 = pl
     return pl
-    
 fl  = np.empty(shape = (1 + lmx))
 z11  = np.empty(shape = (1 + lmx))
 f  = np.empty(shape = (1 + lmx))
@@ -168,11 +168,9 @@ fp  = np.empty(shape = (1 + lmx))
 gp  = np.empty(shape = (1 + lmx))
 phase  = np.empty(shape = (1 + lmx)) 
 sigma = np.empty(shape = (1 + lmx))
-
 for i in range(lmin1, lmx + 1, lspc):
     fl[i] = float(i * (i-1)) / rmu2
     z11[i] = 10 ** 20
-
 for i in range(lmin1, lmx + 1, lspc):
     r=rstart
     for j in range(1, nos + 1, 2):
@@ -193,34 +191,27 @@ for i in range(lmin1, lmx + 1, lspc):
        u11=vv11-ccen*fl[i]
        den=1.0+y11
        z11[i]=y11/den-u11
-
 for i in range(lmin1, lmx + 1, lspc):
     u11=vv11-ccen*fl[i]
     z11[i]=(z11[i]+u11/2.0)/space
-
 if lmax < 1000:
     for i in range(0, 2):
         cz = i + 1.0 + ci*etahyp
-        cg = cgamma(cz)
+        cg = cgammaln(cz)
         sigmal = cmath.phase(cg) 
         f[i], g[i], fp[i], gp[i] = hyper(i, rho, etahyp, sigmal)
     for i in range(1, lmax):
         lm1 = i - 1
         lp1 = i + 1
-
         term1 = ( i + lp1 ) * ( etahyp + ( i * lp1 / rho ) )
         term2 = lp1 * math.sqrt( i * i + etahyp2 )
         term3 = 1.0 / ( i * math.sqrt( lp1 * lp1 + etahyp2 ) )
-
         f[lp1] = ( term1 * f[i] - term2 * f[lm1] ) * term3
         g[lp1] = ( term1 * g[i] - term2 * g[lm1] ) * term3
-
         term1 = math.sqrt( lp1*lp1 + etahyp2 ) / lp1
         term2 = ( lp1*lp1 / rho + etahyp) / lp1
-
         fp[lp1] = term1 * f[i] - term2 * f[lp1]
         gp[lp1] = term1 * g[i] - term2 * g[lp1]
-
     hcon = 4.0*pi/p1/p1
     tcs=0.0
     phase_file = open("Iteration 03/Phase shifts.txt", "w")
@@ -234,7 +225,8 @@ if lmax < 1000:
         phase[i] = math.atan( (logder*fhyp-dfhyp)/(dghyp-ghyp*logder) )
         cg = complex(1.0,0.0) 		
         cz = l + 1.0 + ci*etahyp
-        cg = cgamma(cz)  
+        cg = cgammaln(cz)
+        print(l, cg)
         sigma[i] = cmath.phase(cg)
         phase_file.write(str(l) + "   " + str(phase[i]) + "   " + str(sigma[i]) + "\n")
         tcs = tcs + hcon * (2.0*l+1) * math.sin(phase[i])**2    
@@ -245,7 +237,7 @@ if lmax < 1000:
         sint22 = (math.sin(ang / 2)) ** 2
         ruther = -zasy / ( 2.0 * p1*p1 * sint22 )
         ruthzm =  zmod / ( 2.0 * p1*p1 * sint22 )
-        cratio = cgamma( 1.0+ci*etahyp ) / cgamma( 1.0-ci*etahyp )
+        cratio = cgammaln( 1.0+ci*etahyp ) / cgammaln( 1.0-ci*etahyp )
         cdexpon = cmath.exp( -1*ci*etahyp*math.log(sint22) )
         cfc = ruther * cratio * cdexpon
         cost = math.cos(ang)
@@ -258,3 +250,5 @@ if lmax < 1000:
         dcs = abs(cf)**2
         crosssection_file.write(str(theta) + "   " +  str(dcs) + "   " +  str(ruther**2) + "   " + str(ruthzm**2) + "\n")
     crosssection_file.close()
+else:
+    print("Maximum l should be < 1000")
