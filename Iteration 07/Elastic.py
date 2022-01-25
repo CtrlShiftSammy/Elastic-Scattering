6147e9ccabecb1fb06ebc8f745508b6543a928ea
Kaustav B <76795611+CtrlShiftSammy@users.noreply.github.com>
2022-01-26 02:46:17 +0530

    Update Elastic.py

--- Iteration 07/Elastic.py
+++ Iteration 07/Elastic.py
@@ -76,6 +76,7 @@ for i in range(1, data_lines):
     data[data_lines-i-1, 3] = data[data_lines-i, 3] + 4 * pi * ((data[data_lines-i, 0]) ** 2 - (data[data_lines-i-1, 0]) ** 2) * (data[data_lines-i, 1] + data[data_lines-i-1, 1]) / 4.0
 for i in range(1, data_lines):
     data[i, 4] = (zmod - data[i, 2]) / data[i, 0] - data[i, 3]
+    data[i, 5] = 0.5 * ((en - data[i, 4]) - ((en - data[i, 4]) ** 2 + 4 * pi * data[i, 1]) ** 0.5)
 print(str(data[data_lines - 1, 2]))
 #0 is radius
 #1 is rho
